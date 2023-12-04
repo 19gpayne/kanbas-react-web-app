05c8dd48a481f46b148e5bb6bb19d0c6913681ff
Gwendolyn Payne <gwenpayne2012@yahoo.com>
2023-12-04 12:44:14 -0500

    fix url

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const BASE_API = process.env.REACT_APP_BASE_API_URL || "http://localhost:4000";
-export const USERS_API = `${BASE_API}/api/users`;
+export const BASE_API = process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
+export const USERS_API = `${BASE_API}/users`;
 
 const request = axios.create({
   withCredentials: true,
